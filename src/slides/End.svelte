e444a8c6b786dc463c944bdad72b09ebc7f05f91
Rob Bateman <rob@infiniteturtles.co.uk>
2026-09-24 01:46:31 +0100

    initial commit

---
+++ src/slides/End.svelte
@@ -0,0 +1,11 @@
+<section data-banner="default" data-watermark="true" data-background-image="assets/talk_background_end.png" data-background-opacity="0.6" style="filter: brightness(1.6);">
+    <img style="margin: 0px;" src="assets/thank_you.png" alt="An Emulator's Journey" height="400" width="1200">
+    <div class="r-hstack">
+        <img style="margin: 0px 100px 0px 100px;" src="assets/logo-poki.png" alt="Poki" width="200" height="200">
+        <img style="margin: 0px 100px 0px 100px;" src="assets/logo-coolmathgames.png" alt="Coolmath Games" width="300" height="300">
+    </div>
+    <div class="r-stretch">
+    </div>
+    <p>@robnet</p>
+    <p>http://awayfl.org</p>
+</section>
---
+++ src/slides/Title.svelte
@@ -0,0 +1,7 @@
+<section data-watermark="true" data-background-image="assets/title_background_noloop.gif" data-background-opacity="0.6" style="filter: brightness(1.6);">
+    <img src="assets/title_an_emulators_journey.png" alt="An Emulator's Journey" height="600" width="1200">
+    <div class="r-stretch">
+    </div>
+    <p>@robnet</p>
+    <p>http://awayfl.org</p>
+</section>
